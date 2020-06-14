34adaceb7cad2534e1fcbb55bad127f0ace02224
njit_77 <512424964@qq.com>
2020-06-14 13:13:21 +0800

    misc:score:0.1585

--- zhengqi_model.py
+++ zhengqi_model.py
@@ -18,29 +18,43 @@ TEST_DATA_PATH = './data/zhengqi_test.txt'
 
 def LoadData():
     # load data from txt file
-    train_data = pd.read_table(TRAIN_DATA_PATH)
-    test_data = pd.read_table(TEST_DATA_PATH)
-    train_data_x = train_data.values[:, 0:-1]
-    train_data_y = train_data.values[:, -1]
-    return (train_data_x, train_data_y, test_data)
+    train_data = pd.read_csv(TRAIN_DATA_PATH, sep='\t')
+    test_data = pd.read_csv(TEST_DATA_PATH, sep='\t')
+    return (train_data, test_data)
 
 def main():
-    (train_data_x, train_data_y, test_data) = LoadData()
+    (train_data, test_data) = LoadData()
 
-    pca1 = pca.PCA(n_components=0.95)
-    pca1.fit(train_data_x)
-    train_data_x = pca1.transform(train_data_x)
-    test_data = pca1.transform(test_data)
+    """
+                   本地测试(mse)         线上score        时间 
+    False True  0.10895188423219443      0.1682     2019.1.19 20:00
+    True  True  0.11475080628985444      0.1585     2019.1.20 20:00
+    True  False 0.09424565768574228     
+    False False 0.08918637637655073
+    """
+    if True:
+        train_data.drop(['V5', 'V17', 'V28', 'V22', 'V11', 'V9'], axis=1, inplace=True)
+        test_data.drop(['V5', 'V17', 'V28', 'V22', 'V11', 'V9'], axis=1, inplace=True)
+
+    train_data_x = train_data.drop(['target'], axis=1)
+    train_data_y = train_data['target']
+
+    if True:
+        pca1 = pca.PCA(n_components=0.95)
+        pca1.fit(train_data_x)
+        train_data_x = pca1.transform(train_data_x)
+        test_data = pca1.transform(test_data)
 
     X_train, X_test, Y_train, Y_test = train_test_split(train_data_x, train_data_y, test_size=0.2, random_state=40)
-    myGBR = GradientBoostingRegressor(alpha=0.9, criterion='friedman_mse', init=None,
-                                      learning_rate=0.03, loss='huber', max_depth=14,
-                                      max_features='sqrt', max_leaf_nodes=None,
-                                      min_impurity_decrease=0.0, min_impurity_split=None,
-                                      min_samples_leaf=10, min_samples_split=40,
-                                      min_weight_fraction_leaf=0.0, n_estimators=300,
-                                      presort='auto', random_state=10, subsample=0.8, verbose=0,
-                                      warm_start=False)
+    myGBR = GradientBoostingRegressor(learning_rate=0.03,
+                                      loss='huber',
+                                      max_depth=14,
+                                      max_features='sqrt',
+                                      min_samples_leaf=10,
+                                      min_samples_split=40,
+                                      n_estimators=300,
+                                      random_state=10,
+                                      subsample=0.8)
     myGBR.fit(X_train, Y_train)
     Y_pred = myGBR.predict(X_test)
     print(mean_squared_error(Y_test, Y_pred))
@@ -50,7 +64,6 @@ def main():
 
     res_pd = pd.DataFrame(test_data_y, columns=['target'])
     res_pd.to_csv("./submit/njit_77.txt", index=False, header=False)
-    print("over")
 
 if __name__ == '__main__':
     main()
