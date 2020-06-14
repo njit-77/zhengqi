c2e4e5f843d90ef3088456f9be9cfa75cffeb25d
njit_77 <512424964@qq.com>
2020-06-14 13:19:19 +0800

    misc:score:0.1273;

--- zhengqi_model.py
+++ zhengqi_model.py
@@ -6,11 +6,10 @@
 Have a nice day!
 """
 
-import numpy as np
-import pandas as pd
+
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
-from sklearn.decomposition import pca
+import pandas as pd  # 导入pandas库
 from sklearn.ensemble import GradientBoostingRegressor
 
 TRAIN_DATA_PATH = './data/zhengqi_train.txt'
@@ -23,27 +22,30 @@ def LoadData():
     test_data = pd.read_csv(TEST_DATA_PATH, sep='\t')
     return (train_data, test_data)
 
-def NormalizeData(train_data):
-    mean = np.mean(train_data, axis=0)
-    std = np.std(train_data, axis=0)
-    return (train_data - mean)/std
-
 def main():
     (train_data, test_data) = LoadData()
+
+    train_data.drop(['V27'], axis=1, inplace=True)
+    test_data.drop(['V27'], axis=1, inplace=True)
 
     train_data_x = train_data.drop(['target'], axis=1)
     train_data_y = train_data['target']
 
-    train_data_x = NormalizeData(train_data_x)
-
-    X_train, X_test, Y_train, Y_test = train_test_split(train_data_x, train_data_y, test_size=0.2, random_state=40)
-    params = {'learning_rate': 0.03, 'loss': 'huber', 'max_depth': 14, 'max_features': 'sqrt',
-              'min_samples_leaf': 10, 'min_samples_split': 40, 'n_estimators': 300,
-              'random_state': 10, 'subsample': 0.8}
-    clf = GradientBoostingRegressor(**params)
-    clf.fit(X_train, Y_train)
-    Y_pred = clf.predict(X_test)
-    print(mean_squared_error(Y_test, Y_pred))
+    if True:
+        X_train, X_test, Y_train, Y_test = train_test_split(train_data_x, train_data_y, test_size=0.2, random_state=40)
+        params = {'learning_rate': 0.03,
+                  'loss': 'huber',
+                  'max_depth': 14,
+                  'max_features': 'sqrt',
+                  'min_samples_leaf': 10,
+                  'min_samples_split': 40,
+                  'n_estimators': 300,
+                  'random_state': 10,
+                  'subsample': 0.8}
+        clf = GradientBoostingRegressor(**params)
+        clf.fit(X_train, Y_train)
+        Y_pred = clf.predict(X_test)
+        print(mean_squared_error(Y_test, Y_pred))
 
     '''结果预测'''
     if True:
@@ -53,4 +55,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
